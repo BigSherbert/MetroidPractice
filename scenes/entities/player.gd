159b4994a0a0b586961be9fa728d88255d456140
BigSherbert <johnsonkkarl@gmail.com>
2026-07-22 22:00:30 -0500

    Player Development
    Adding Movement and Gravity for Player Scene

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -1,12 +1,35 @@
 extends CharacterBody2D
 
 #Character Physics
-#Gravity is stored in Physics Setting of Godot, we will call it later.
-var speed = 100
-var jump_velocity = -450
+var playerspeed : int = 100
+var playerjump_velocity : int = -450
+#Global Gravity is stored in Project Settings (Physics) of Godot so we could call that or we could state explicitly.
+#Calling Gravity
+#var playergravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#Stating Gravity Explicitly
+var playergravity : float = 981.0
 
 func _physics_process(delta: float) -> void:
 	
-	#Lets handle gravity first
+	#If the player is not on a platform, use gravity.
 	if !is_on_floor():
-		velocity.y += ProjectSettings.get
+		velocity.y += playergravity * delta
+	
+	#If the player presses jump move the character up on y axis, but only if they are on a platform.
+	if Input.is_action_just_pressed("jump") and is_on_floor() :
+		velocity.y += playerjump_velocity
+	
+	#Gravity will handle Y Axis, so we only need to worry about X axis movement.
+	#We will store axis direction as a variable so we can test it every process.
+	var playerdirection = Input.get_axis("left","right")
+	
+	if playerdirection :
+		velocity.x = playerdirection * playerspeed
+	else:
+		#Clunky movement
+		#velocity.x = 0.0
+		#Better Movement
+		velocity.x = move_toward(velocity.x, 0, playerspeed)
+	
+	move_and_slide()
+	

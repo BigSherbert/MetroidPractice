ad0892f1aa24e8288dcec3ae49d89ad2c6ea3aaf
BigSherbert <johnsonkkarl@gmail.com>
2026-07-24 09:27:51 -0500

    Animation Work
    Updated Torso Sprite Sheet for longer legs.
    Added basic Torso, working on mouse controlled animation.
    Added @export for bullet offset to make future offset adjustment easy.

--- scenes/bullets/bullet.gd
+++ scenes/bullets/bullet.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var direction : Vector2
 @export var speed : = 500
+@export var offset : = 16
 
 #Instead of trying to explicitly state properties elsewhere, we just send them in a simple function!
 #This is helpful because we might want specific adjustments for scenes/characters
@@ -9,7 +10,7 @@ var direction : Vector2
 func setup(pos: Vector2, dir : Vector2 ) :
 	#Here we want the starting position of the bullet to be the character position adding the direction and moving
 	#it away from center by 16
-	position = pos + dir * 16
+	position = pos + dir * offset
 	direction = dir
 
 func _physics_process(delta: float) -> void:
--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 #Character Physics
 #@export allows us to expose these variables to the inspector
-var playerspeed : int = 100
+@export var playerspeed : int = 100
 @export var jump_velocity : int = -450
 #Global Gravity is stored in Project Settings (Physics) of Godot so we could call that or we could state explicitly.
 #Calling Gravity
@@ -17,31 +17,33 @@ signal shoot(pos: Vector2, dir: Vector2)
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-
+	
 	var playerdirection := Input.get_axis("left", "right")
-
+	
 	if playerdirection != 0:
 		velocity.x = playerdirection * playerspeed
 		$Legs.flip_h = playerdirection < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, playerspeed)
-
+	
 	move_and_slide()
-
-	# Choose animation after movement.
+	
+	# Choose Legs animation after movement.
 	if !is_on_floor():
 		$AnimationPlayer.play("jumping")
 	elif playerdirection != 0:
 		$AnimationPlayer.play("run")
 	else:
 		$AnimationPlayer.play("idle")
+	
+	#Choose Torso animation after movement but before shooting
+	var torsodirection = get_local_mouse_position().normalized()
+	var adjustedtorsodirection = Vector2(round(torsodirection.x),round(torsodirection.y))
+	print(adjustedtorsodirection)
 
 	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0.0:
-		shoot.emit(
-			global_position,
-			(get_global_mouse_position() - global_position).normalized()
-		)
+		shoot.emit(global_position,(get_global_mouse_position() - global_position).normalized())
 		$ReloadTimer.start()

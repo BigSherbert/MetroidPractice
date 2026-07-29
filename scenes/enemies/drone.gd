d367cf54af305bbd4c4c022187affb97c4b5180e
BigSherbert <johnsonkkarl@gmail.com>
2026-07-28 19:50:21 -0500

    Drones Building

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -28,3 +28,15 @@ func _on_detection_area_body_entered(detectedplayer: CharacterBody2D) -> void:
 #If player runs away and leaves detection area, stop the drone.
 func _on_detection_area_body_exited(_detectedplayer: CharacterBody2D) -> void:
 	player = null
+
+#Explode on impact with Player
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Play Explosion")
+	$DroneCollisionArea.set_deferred("disabled", true)
+	$AnimatedSprite2D.visible = false
+	$AnimationPlayer.play("explode")
+
+#Remove the drone when animation finished.
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	print("Remove Drone")
+	queue_free()

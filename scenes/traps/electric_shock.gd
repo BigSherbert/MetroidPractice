6406248820551349c300f6e486311babe59eba41
BigSherbert <johnsonkkarl@gmail.com>
2026-08-01 15:36:38 -0500

    Electric Trap No Signals

--- scenes/traps/electric_shock.gd
+++ scenes/traps/electric_shock.gd
@@ -1,10 +1,19 @@
 extends Area2D
 
-func _ready():
+func _ready() -> void:
 	play_random()
 
-func play_random():
+
+func _process(_delta: float) -> void:
+	if $ElectricEffect.is_playing():
+		$BlueLight.energy = randf_range(0.4, 1.0)
+
+func play_random() -> void:
+	$BlueLight.visible = true
+	$BlueLight.energy = 0.8
+	$Zap.play()
 	$ElectricEffect.play("electrifying")
 	await $ElectricEffect.animation_finished
-	await get_tree().create_timer(randf_range(0.3, 2.0)).timeout
+	$BlueLight.visible = false
+	await get_tree().create_timer(randf_range(0.3, 1.5)).timeout
 	play_random()
